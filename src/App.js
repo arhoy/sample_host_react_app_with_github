8b2389930f1f9f8f599c4902c54de19496d15fc1
aquasar <arhoy@ualberta.ca>
2019-06-17 17:15:27 -0600

    changed message

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Host React App with GITHUB pages
         </a>
       </header>
     </div>
